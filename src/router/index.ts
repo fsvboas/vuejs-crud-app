f32a78434a8929c041d5d7b42a09ef3887820c1c
fsvboas <fsvboas.dev@gmail.com>
2024-09-07 20:27:09 -0300

    feat: create new views/routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MyCategoriesView from '../views/MyCategories.vue'
+import MySubcategoriesView from '../views/MySubcategories.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/my-categories',
+      name: 'my-categories',
+      component: MyCategoriesView
+    },
+    {
+      path: '/my-subcategories',
+      name: 'my-subcategories',
+      component: MySubcategoriesView
     }
   ]
 })
---
+++ src/views/MyCategories.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ContentContainer from '@/components/ContentContainer.vue'
+</script>
+
+<template>
+  <ContentContainer>
+    <h1>My categories</h1>
+  </ContentContainer>
+</template>
