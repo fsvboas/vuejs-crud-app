0dd026f87a4d3e37e48d8d711bdad29d70dfb20d
fsvboas <fsvboas.dev@gmail.com>
2024-09-07 20:27:48 -0300

    feat: update 'App' file and create a initial layout to 'Home View'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
+
+const route = useRoute()
+
+console.log(route.name)
+
+const headerTitle = computed(() =>
+  route.name === 'home' || route.name === 'my-categories'
+    ? 'Categorias'
+    : route.name === 'my-subcategories'
+      ? 'Subcategorias'
+      : undefined
+)
+
+const headerHasGoBackButton = computed(() => (route.name === 'home' ? false : true))
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen bg-white text-dark-blue font-medium">
+    <AppHeader :title="headerTitle" :has-go-back-button="headerHasGoBackButton" />
+    <RouterView />
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
-import { getCategories } from '@/services/get-categories'
-import { useQuery } from '@tanstack/vue-query'
-import { PersonStanding } from 'lucide-vue-next'
+import ContentContainer from '@/components/ContentContainer.vue'
 import { NButton } from 'naive-ui'
+import { RouterLink } from 'vue-router'
 
-const { data } = useQuery({
-  queryKey: ['categories'],
-  queryFn: () => getCategories()
-})
+// const { data } = useQuery({
+//   queryKey: ['categories'],
+//   queryFn: () => getCategories()
+// })
 
-const categories = data.value || []
+// const categories = data.value || []
 </script>
 
 <template>
-  <main>
-    <div class="flex items-center justify-center min-h-screen space-x-2">
-      <h1 class="text-green-500 font-medium text-2xl">Hello World</h1>
-      <PersonStanding :size="32" class="text-green-500" />
-      <n-button>Naive UI Button</n-button>
-      <ul>
-        <li v-for="category in categories" :key="category.id">{{ category.name }}</li>
-      </ul>
+  <ContentContainer>
+    <div>
+      <div class="mb-6">
+        <h3 class="text-xl leading-6 font-semibold mb-3 text-dark-blue">
+          Organize seu catálogo com categorias e subcategorias
+        </h3>
+        <p class="leading-5 text-grayscale-pure-gray">
+          Ao usar as categorias e subcategorias do catálogo, você ajuda seus clientes a encontrarem
+          os produtos desejados.
+        </p>
+      </div>
+      <n-button
+        class="rounded-full !bg-brand-light-pink gap-2 py-[10px] px-4 text-base !text-brand-pure-pink"
+        quaternary
+      >
+        <RouterLink to="/my-categories">
+          Criar categorias
+          <UIcon name="i-lucide:plus" class="w-4 h-4 text-brand-pure-pink" />
+        </RouterLink>
+      </n-button>
     </div>
-  </main>
+  </ContentContainer>
 </template>
