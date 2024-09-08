798a1e9f7e3545b7b3ac5548cd83bc7e7245be3a
fsvboas <fsvboas.dev@gmail.com>
2024-09-07 22:41:58 -0300

    feat: create 'GenericButton' component

---
+++ src/components/core/GenericButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { buttonProps, NButton } from 'naive-ui'
+
+const props = defineProps({
+  ...buttonProps
+})
+</script>
+
+<template>
+  <n-button v-bind="props" strong :focusable="false" class="duration-500">
+    <slot />
+  </n-button>
+</template>
