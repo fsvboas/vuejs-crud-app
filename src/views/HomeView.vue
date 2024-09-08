568fd87a51de9acad0e3ea13a6827107eddd022e
fsvboas <fsvboas.dev@gmail.com>
2024-09-08 04:05:58 -0300

    refactor: minor adjustments to "HomeView'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import ContentContainer from '@/components/ContentContainer.vue'
 import GenericButton from '@/components/core/GenericButton.vue'
 import { LucidePlus } from 'lucide-vue-next'
+import { NIcon } from 'naive-ui'
 
 // const { data } = useQuery({
 //   queryKey: ['categories'],
@@ -13,23 +14,30 @@ import { LucidePlus } from 'lucide-vue-next'
 
 <template>
   <ContentContainer>
-    <div>
-      <div class="mb-6">
-        <h3 class="text-xl leading-6 font-semibold mb-3 text-dark-blue">
-          Organize seu catálogo com categorias e subcategorias
-        </h3>
-        <p class="leading-5 text-greyscale-pure-grey">
-          Ao usar as categorias e subcategorias do catálogo, você ajuda seus clientes a encontrarem
-          os produtos desejados.
-        </p>
-      </div>
-
-      <RouterLink to="/my-categories" v-slot="{ navigate }">
-        <GenericButton round color="#FFE2EB" text-color="#DA3468" role="link" @click="navigate">
-          Criar categoria
-          <LucidePlus class="w-4 h-4 ml-2" />
-        </GenericButton>
-      </RouterLink>
+    <div class="mb-6">
+      <h3 class="text-xl leading-6 font-semibold mb-3 text-dark-blue">
+        Organize seu catálogo com categorias e subcategorias
+      </h3>
+      <p class="leading-5 text-greyscale-pure-grey">
+        Ao usar as categorias e subcategorias do catálogo, você ajuda seus clientes a encontrarem os
+        produtos desejados.
+      </p>
     </div>
+
+    <RouterLink to="/my-categories" v-slot="{ navigate }" class="w-fit">
+      <GenericButton
+        round
+        color="#FFE2EB"
+        text-color="#DA3468"
+        role="link"
+        @click="navigate"
+        class="h-11 w-44"
+      >
+        <span class="mr-2 text-base">Criar categoria</span>
+        <n-icon size="16">
+          <LucidePlus />
+        </n-icon>
+      </GenericButton>
+    </RouterLink>
   </ContentContainer>
 </template>
