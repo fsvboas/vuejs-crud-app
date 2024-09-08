cab7c7e6f2ac4192c32091ea9c5fac2f258b0af1
fsvboas <fsvboas.dev@gmail.com>
2024-09-07 22:43:36 -0300

    refactor: implement 'GenericButton' component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import ContentContainer from '@/components/ContentContainer.vue'
+import GenericButton from '@/components/core/GenericButton.vue'
 import { LucidePlus } from 'lucide-vue-next'
-import { NButton } from 'naive-ui'
 
 // const { data } = useQuery({
 //   queryKey: ['categories'],
@@ -18,22 +18,17 @@ import { NButton } from 'naive-ui'
         <h3 class="text-xl leading-6 font-semibold mb-3 text-dark-blue">
           Organize seu catálogo com categorias e subcategorias
         </h3>
-        <p class="leading-5 text-grayscale-pure-gray">
+        <p class="leading-5 text-greyscale-pure-grey">
           Ao usar as categorias e subcategorias do catálogo, você ajuda seus clientes a encontrarem
           os produtos desejados.
         </p>
       </div>
 
       <RouterLink to="/my-categories" v-slot="{ navigate }">
-        <n-button
-          class="bg-brand-light-pink rounded-full text-brand-pure-pink !font-medium h-11 w-44"
-          quaternary
-          role="link"
-          @click="navigate"
-        >
+        <GenericButton round color="#FFE2EB" text-color="#DA3468" role="link" @click="navigate">
           Criar categoria
           <LucidePlus class="w-4 h-4 ml-2" />
-        </n-button>
+        </GenericButton>
       </RouterLink>
     </div>
   </ContentContainer>
