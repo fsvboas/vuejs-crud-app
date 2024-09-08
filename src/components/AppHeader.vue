2a448965d3e38a2a2abbfcb592f800b4534a4e56
fsvboas <fsvboas.dev@gmail.com>
2024-09-07 22:42:47 -0300

    refactor: minor adjustments to 'AppHeader'

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,7 +20,7 @@ const props = defineProps({
   >
     <RouterLink
       to="/"
-      class="fixed flex w-6 h-6 items-center justify-center"
+      class="fixed flex w-8 h-8 items-center justify-center"
       v-if="props.hasGoBackButton"
     >
       <LucideArrowLeft class="text-brand-pure-pink h-4 w-4" />
