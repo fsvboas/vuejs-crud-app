b5288e5fc468af1a9654a230fde16c1b0ccf9642
fsvboas <fsvboas.dev@gmail.com>
2024-09-09 20:03:50 -0300

    chore: setup 'Pinia' library

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { queryClient } from './libs/tanstack-query/query-client'
 import './main.css'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 
 app.use(VueQueryPlugin, { queryClient: queryClient })
+
+app.use(pinia)
 
 app.mount('#app')
