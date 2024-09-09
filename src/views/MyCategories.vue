ccdebb5900670064d7d42464aee842a22b29a1a1
fsvboas <fsvboas.dev@gmail.com>
2024-09-09 02:29:22 -0300

    feat: create 'InputCardSkeletonPlaceholder' component

--- src/views/MyCategories.vue
+++ src/views/MyCategories.vue
@@ -3,6 +3,7 @@ import ContentContainer from '@/components/ContentContainer.vue'
 import CreateCategoryForm from '@/components/CreateCategoryForm.vue'
 import EmptyState from '@/components/EmptyState.vue'
 import InputCard from '@/components/core/InputCard.vue'
+import InputCardSkeletonPlaceholder from '@/components/skeletons/InputCardSkeletonPlaceholder.vue'
 import { getCategories } from '@/services/get-categories'
 import { useQuery } from '@tanstack/vue-query'
 import { computed } from 'vue'
@@ -19,15 +20,16 @@ const categories = computed(() => data.value || [])
   <ContentContainer>
     <CreateCategoryForm />
     <div class="flex w-full justify-center my-4">
-      <div class="w-full space-y-3" v-if="categories.length">
+      <div class="w-full space-y-3" v-if="categories.length && !isLoading">
         <InputCard
           v-for="category in categories"
           :categoryName="category.name"
           v-bind:key="category.id"
         />
       </div>
-      <!-- TO-DO: PENDING (SKELETON PLACEHOLDER) -->
-      <div v-else-if="isLoading === true">CARREGANDO</div>
+      <div class="w-full space-y-3" v-else-if="isLoading">
+        <InputCardSkeletonPlaceholder />
+      </div>
       <div v-else><EmptyState /></div>
     </div>
   </ContentContainer>
