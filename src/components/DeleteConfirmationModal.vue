7e2d56b0832194f7877a61ec779bddb77a6d9d12
fsvboas <fsvboas.dev@gmail.com>
2024-09-10 17:54:02 -0300

    refactor: fix 'DeleteConfirmationModal' responsiveness

--- src/components/DeleteConfirmationModal.vue
+++ src/components/DeleteConfirmationModal.vue
@@ -48,40 +48,46 @@ const handleDeleteCategory = () => {
 </script>
 
 <template>
-  <n-modal v-model:show="showModal" :mask-closable="false">
-    <div class="bg-white rounded-3xl p-4 w-full h-fit mx-7 flex flex-col space-y-4">
-      <img :src="WarningSvg" alt="warning-icon" class="self-center w-20 h-20" />
-      <div class="space-y-2">
-        <h4 class="text-xl font-semibold leading-6 text-dark-blue">Deseja excluir a categoria?</h4>
-        <div>
-          <p class="text-base font-medium leading-5 text-greyscale-pure-grey">
-            Essa ação é irreversível e implica na exclusão das subcategorias vinculadas.
-          </p>
-          <p class="text-base font-medium leading-5 text-greyscale-pure-grey">
-            Os produtos ligados a essa categoria não serão excluídos.
-          </p>
+  <n-modal v-model:show="showModal" :mask-closable="false" transform-origin="center">
+    <div class="max-[380px]:mx-7">
+      <div
+        class="bg-white rounded-3xl p-4 w-full max-w-80 md:max-w-96 h-fit flex flex-col space-y-4"
+      >
+        <img :src="WarningSvg" alt="warning-icon" class="self-center w-20 h-20" />
+        <div class="space-y-2">
+          <h4 class="text-xl font-semibold leading-6 text-dark-blue">
+            Deseja excluir a categoria?
+          </h4>
+          <div>
+            <p class="text-base font-medium leading-5 text-greyscale-pure-grey">
+              Essa ação é irreversível e implica na exclusão das subcategorias vinculadas.
+            </p>
+            <p class="text-base font-medium leading-5 text-greyscale-pure-grey">
+              Os produtos ligados a essa categoria não serão excluídos.
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="flex flex-row space-x-2">
-        <GenericButton
-          round
-          color="#FFE2EB"
-          text-color="#DA3468"
-          role="link"
-          class="flex-1 h-11 disabled:bg-greyscale-light-grey disabled:text-greyscale-dark-grey"
-          :disabled="pendingDeleteCategory"
-          @click="closeModal"
-        >
-          Cancelar
-        </GenericButton>
-        <GenericButton
-          round
-          color="#DA3468"
-          class="flex-1 h-11"
-          @click="handleDeleteCategory"
-          :loading="pendingDeleteCategory"
-          >Excluir</GenericButton
-        >
+        <div class="flex flex-row space-x-2">
+          <GenericButton
+            round
+            color="#FFE2EB"
+            text-color="#DA3468"
+            role="link"
+            class="flex-1 h-11 disabled:bg-greyscale-light-grey disabled:text-greyscale-dark-grey"
+            :disabled="pendingDeleteCategory"
+            @click="closeModal"
+          >
+            Cancelar
+          </GenericButton>
+          <GenericButton
+            round
+            color="#DA3468"
+            class="flex-1 h-11"
+            @click="handleDeleteCategory"
+            :loading="pendingDeleteCategory"
+            >Excluir</GenericButton
+          >
+        </div>
       </div>
     </div>
   </n-modal>
