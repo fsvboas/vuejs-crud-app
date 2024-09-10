dec26b22c98d80e4b2bff8dc5dc5bf61876dde80
fsvboas <fsvboas.dev@gmail.com>
2024-09-10 16:58:51 -0300

    feat: 'MySubcategories' initial layout/integration

---
+++ src/components/CreateSubcategoryForm.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { queryClient } from '@/libs/tanstack-query/query-client'
+import { postCategory } from '@/services/post-category'
+import type { CategoryType } from '@/types/category-type'
+import { useMutation } from '@tanstack/vue-query'
+import { NForm, NFormItem } from 'naive-ui'
+import AddCategoryInputCard from './AddCategoryInputCard.vue'
+
+const props = defineProps<{
+  parentId: CategoryType['id']
+}>()
+
+const { mutate: create, isPending: pendingCreateSubcategory } = useMutation({
+  mutationFn: postCategory,
+  onSuccess: () => {
+    // TO-DO: TOAST NOTIFICATION
+    queryClient.invalidateQueries({ queryKey: ['categories'] })
+  },
+  onError: () => {
+    // TO-DO: TOAST NOTIFICATION
+  }
+})
+
+const handleSubmitForm = (categoryName: CategoryType['name']) => {
+  create({
+    name: categoryName,
+    parent: {
+      id: props.parentId
+    }
+  })
+}
+</script>
+
+<template>
+  <n-form class="flex w-full h-11 space-x-2" :show-label="false">
+    <!-- TO-DO: change the input border color -->
+    <n-form-item class="w-full" :show-feedback="false" path="create-subcategory">
+      <AddCategoryInputCard
+        @submitForm="handleSubmitForm"
+        :pendingCreateCategory="pendingCreateSubcategory"
+      />
+    </n-form-item>
+  </n-form>
+</template>
--- src/views/MySubcategories.vue
+++ src/views/MySubcategories.vue
@@ -1,9 +1,66 @@
 <script setup lang="ts">
 import ContentContainer from '@/components/ContentContainer.vue'
+import Accordion from '@/components/core/Accordion.vue'
+import CreateSubcategoryForm from '@/components/CreateSubcategoryForm.vue'
+import InputCardSkeletonPlaceholder from '@/components/skeletons/InputCardSkeletonPlaceholder.vue'
+import UpdateCategoryForm from '@/components/UpdateCategoryForm.vue'
+import { getCategories } from '@/services/get-categories'
+import type { CategoryType } from '@/types/category-type'
+import { useQueries, useQuery } from '@tanstack/vue-query'
+import { computed } from 'vue'
+
+const { data: categoriesData, isLoading: isLoadingCategoriesData } = useQuery({
+  queryKey: ['categories'],
+  queryFn: () => getCategories({})
+})
+
+const categories = computed(() => categoriesData.value || [])
+const hasSubcategories = computed(() =>
+  categories.value.filter((category) => category.hasChildren === true)
+)
+const subcategoryIds = computed(() => hasSubcategories.value.map((category) => category.id))
+
+const subcategoriesResults = useQueries({
+  queries: subcategoryIds.value.map((parentCategoryId) => ({
+    queryKey: ['subcategory', parentCategoryId],
+    queryFn: () => getCategories({ parentCategoryId })
+  }))
+})
+
+const subcategoriesValue = computed(() => subcategoriesResults.value || [])
+const subcategoriesIsLoading = computed(() =>
+  subcategoriesValue.value.some((subcategory) => subcategory.isLoading)
+)
+
+const subcategoriesDataMap = computed(() => {
+  if (!subcategoriesValue.value) return
+  return subcategoriesValue.value.flatMap((subcategory) => subcategory.data || [])
+})
+
+const subcategoriesFilter = (parentId: CategoryType['id']) => {
+  if (!subcategoriesDataMap.value) return
+
+  return subcategoriesDataMap.value.filter((subcategory) => subcategory?.parent?.id === parentId)
+}
 </script>
 
 <template>
   <ContentContainer>
-    <h1>My subcategories</h1>
+    <h2 class="text-base text-[#737F9C] leading-5 mb-4">
+      Adicione as subcategorias abaixo das categorias pai
+    </h2>
+    <div class="w-full space-y-3" v-if="!isLoadingCategoriesData && !subcategoriesIsLoading">
+      <Accordion v-for="category in categories" v-bind:key="category.id" :title="category.name">
+        <CreateSubcategoryForm :parent-id="category.id" />
+        <UpdateCategoryForm
+          v-for="subcategory in subcategoriesFilter(category.id)"
+          v-bind:key="subcategory?.id"
+          :category="subcategory"
+        />
+      </Accordion>
+    </div>
+    <div class="w-full space-y-3" v-else>
+      <InputCardSkeletonPlaceholder />
+    </div>
   </ContentContainer>
 </template>
