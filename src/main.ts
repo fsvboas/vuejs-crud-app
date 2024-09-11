082677e2151169adf7d3eed4b6b31fd5b611c20f
fsvboas <fsvboas.dev@gmail.com>
2024-09-10 23:35:19 -0300

    chore: add vue-toast-notification lib and create a hook

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { VueQueryPlugin } from '@tanstack/vue-query'
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-default.css'
 import App from './App.vue'
 import { queryClient } from './libs/tanstack-query/query-client'
 import './main.css'
 import router from './router'
 
-const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 
 app.use(VueQueryPlugin, { queryClient: queryClient })
 
-app.use(pinia)
+app.use(ToastPlugin)
 
 app.mount('#app')
